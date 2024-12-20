2a28ab3d8ea7ddcaebd837a2d5de0125c19dfcf9
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-20 12:34:06 +0600

    implementation

--- Sorting Algos/insertion_sort.cpp
+++ Sorting Algos/insertion_sort.cpp
@@ -1,9 +1,40 @@
+// Implementation of Insertion sort in C++
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
+void insertion_sort(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 int main()
 {
-    
+    int size;
+    cin >> size;
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr, size);
+
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
