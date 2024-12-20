b1e84801589c9251918f7aa1cb473c72b34e3176
S.M.Arifuzzaman <smarifuzzaman123@gmail.com>
2024-12-20 11:59:21 +0600

    added elapsed time

--- Sorting Algos/bubble_sort.py
+++ Sorting Algos/bubble_sort.py
@@ -1,4 +1,5 @@
 import random
+import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -18,21 +19,26 @@ def update(frame):
             bar.set_color("red")
         else:
             bar.set_color("blue")
+    elapsed_time_text.set_text(f"Time Elapsed: {time.time() - start_time:.2f}s")
 
 def generate_data(size, max_value):
     return [random.randint(1, max_value) for _ in range(size)]
 
 def visualize_bubble_sort(size=50, max_value=100):
+    global start_time, bar_container, elapsed_time_text
     data = generate_data(size, max_value)
     
     fig, ax = plt.subplots()
     ax.set_title("Bubble Sort Visualization")
     
-    global bar_container
     bar_container = ax.bar(range(len(data)), data, align="center", alpha=0.7, color="blue")
     ax.set_xlim(-1, size)
     ax.set_ylim(0, max_value + 10)
     
+    # Add elapsed time text
+    elapsed_time_text = ax.text(0.02, 0.95, "", transform=ax.transAxes, fontsize=12, verticalalignment='top')
+    
+    start_time = time.time()
     ani = animation.FuncAnimation(
         fig, update, frames=bubble_sort_visualization(data), interval=100, repeat=False
     )
